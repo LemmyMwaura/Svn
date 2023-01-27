07a7877371c9f5344494ae7e4099034f46f3108f
lemmy mwaura <lemminton@gmail.com>
2023-01-27 19:35:12 +0300

    feat: validate route

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,50 @@
+import Image from 'next/image'
+import { GetServerSideProps } from 'next'
+import { getSession, useSession, signOut } from 'next-auth/react'
+
 const Home = () => {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading') {
+    ;('loading details')
+  }
+
+  const handleSignOut = () => {
+    signOut()
+  }
+
   return (
-    <div>home</div>
+    <div>
+      <div>{session?.user?.name}</div>
+      <img
+        src={session?.user?.image}
+        width={100}
+        height={100}
+        alt="User Profile Image"
+      />
+      <button className='btn' onClick={handleSignOut}>Log Out</button>
+      
+      Welcome {JSON.stringify(session)}
+    </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
