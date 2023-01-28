0ddae9e6d3df64df428751f6d82c8b40af15c829
lemmy mwaura <lemminton@gmail.com>
2023-01-28 14:42:00 +0300

    feat: validate signup form

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import { BsEye, BsEyeSlash } from 'react-icons/bs'
 import styles from '@/styles/Auth.module.scss'
 
@@ -6,9 +7,31 @@ interface Props {
   toggle: () => void
 }
 
+type Inputs = {
+  username: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
 const Signup = ({ toggle }: Props) => {
   const [show, setShow] = useState(false)
   const [showcp, setCShow] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const passMatch = (): boolean => {
+    const watchPasswords = watch(['password', 'confirmPassword'])
+    if (watchPasswords[0] === watchPasswords[1]) {
+      return true
+    }
+    return false
+  }
 
   const togglePass = () => {
     setShow((prev) => !prev)
@@ -18,34 +41,75 @@ const Signup = ({ toggle }: Props) => {
     setCShow((prev) => !prev)
   }
 
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+    // reset()
+  }
+
   return (
     <div>
-      <form action="" className={styles.container}>
+      <form
+        onSubmit={() => handleSubmit(onSubmit)}
+        className={styles.container}
+      >
         <h3>SignUp</h3>
         <div className={styles.fields_wrapper}>
-          <input className={styles.input} type="text" placeholder="Username" />
-          <input className={styles.input} type="text" placeholder="Email" />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Username"
+            {...register('username', { required: true })}
+          />
+          {errors.username && (
+            <span className={styles.error}>Your Username is required</span>
+          )}
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Email"
+            {...register('email', { required: true })}
+          />
+          {errors.email && (
+            <span className={styles.error}>Your Email is required</span>
+          )}
           <div className={styles.password}>
             <input
               className={styles.input}
               type={show ? 'text' : 'password'}
               placeholder="Password"
+              {...register('password', { required: true })}
             />
             <span className={styles.icon} onClick={togglePass}>
               {show ? <BsEyeSlash /> : <BsEye />}
             </span>
           </div>
+          {errors.password && (
+            <span className={styles.error}>Your Password is required</span>
+          )}
           <div className={styles.password}>
             <input
               className={styles.input}
               type={showcp ? 'text' : 'password'}
               placeholder="Confirm Password"
+              {...register('confirmPassword', { required: true })}
             />
             <span className={styles.icon} onClick={toggleConfirmPass}>
               {showcp ? <BsEyeSlash /> : <BsEye />}
             </span>
           </div>
-          <button className={styles.login_btn}>Sign Up</button>
+          {errors.confirmPassword && (
+            <span className={styles.error}>Please Confirm Your password</span>
+          )}
+          {!passMatch() && (
+            <span className={styles.error}>Passwords don`t match</span>
+          )}
+          <button
+            disabled={!passMatch()}
+            className={styles.login_btn}
+            type="submit"
+          >
+            Sign Up
+          </button>
         </div>
         <div className={styles.footer}>
           Already have an account<span onClick={toggle}>Login</span>
