f01690fc196262d69f7655455fa4006ec249de97
lemmy mwaura <lemminton@gmail.com>
2023-01-28 15:56:03 +0300

    feat: fetch users

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,47 @@
+import Head from 'next/head'
 import { GetServerSideProps } from 'next'
 import { getSession, useSession } from 'next-auth/react'
+import { prisma } from '@/lib/prisma.util'
+import { Prisma } from '@prisma/client'
+
+// components
 import Profile from '@/components/profile'
 import Spinner from '@/components/spinner'
+
+// styles
 import spinStyles from '@/styles/Utils.module.scss'
 import styles from '@/styles/Home.module.scss'
 
-const Home = () => {
+interface Props {
+  // TODO: improve typesafety
+  users: Prisma.UserSelect[]
+}
+
+const Home = ({ users }: Props) => {
   const { data: session, status } = useSession()
 
   if (status === 'loading') {
     return (
-      <div className={spinStyles.wrapper}>
-        <Spinner />
-      </div>
+      <>
+        <Head>
+          <title>Home | Savannah</title>
+        </Head>
+        <div className={spinStyles.wrapper}>
+          <Spinner />
+        </div>
+      </>
     )
   }
 
   return (
-    <div className={styles.wrapper}>
-      <Profile user={session?.user} />
-    </div>
+    <>
+      <Head>
+        <title>Home | Savannah</title>
+      </Head>
+      <div className={styles.wrapper}>
+        <Profile user={session?.user} />
+      </div>
+    </>
   )
 }
 
@@ -27,6 +49,8 @@ export default Home
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req })
+  const users = await prisma.user.findMany()
+
   if (!session) {
     return {
       redirect: {
@@ -39,6 +63,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   return {
     props: {
       session,
+      users,
     },
   }
 }
