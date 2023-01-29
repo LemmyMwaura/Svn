ca42815ae20433ab633d502ea89b56b6b0e4f3b1
lemmy mwaura <lemminton@gmail.com>
2023-01-29 21:01:06 +0300

    chore: add and setup tanstack-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,23 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from 'react-hot-toast'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <SessionProvider session={session}>
-      <Toaster  position="top-right"/>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Toaster position="top-right" />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
