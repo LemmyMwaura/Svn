acd587b00538496cab3bb7cc492d08ebdead0062
lemmy mwaura <lemminton@gmail.com>
2023-01-29 16:17:17 +0300

    feat(auth): add custom credentials

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import prisma from '@/lib/prisma.util'
 
 export const authOptions = {
   providers: [
@@ -12,8 +14,40 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    CredentialsProvider({
+      name: 'credentials',
+      async authorize(credentials, req) {
+        // check user
+        const { email, password: userPass } = credentials
+        const result = await prisma.user.findFirst({
+          where: { email },
+        })
+        
+        console.log({email, result})
+
+        if (!result) {
+          return res.status(400).json({ message: "Account doesn't exist" })
+        }
+
+        //validate password
+        compare(userPass, user.password, async (err, result) => {
+          if (err) {
+            res.status(500).json({ message: 'Something went wrong, Try again' })
+          }
+
+          if (!result) {
+            return res.status(403).json({ message: 'Password is incorrect' })
+          }
+        })
+        
+        return result
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    jwt: true,
+  },
 }
 
 export default NextAuth(authOptions)
