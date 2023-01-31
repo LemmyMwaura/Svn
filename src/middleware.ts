0c5791285a6ca3d71a11a841b5b2bef6988df855
lemmy mwaura <lemminton@gmail.com>
2023-01-31 15:25:14 +0300

    fix(middleware): vercel ignores the default route, update to fix that

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,20 @@ export const config = {
     /*
      * All routes including the APIs are protected
      * Match all request paths except for the ones starting with:
-     * - index (landingpage)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!/|_next/static|_next/image|favicon.ico).*)',
+    '/_next/static',
+    '/_next/image',
+    '/_next/static',
+    '/favicon.ico',
+    '/user',
+    '/home',
+    '/login',
+    '/album',
+    '/photos',
+    '/api/:path*',
   ],
 }
 
